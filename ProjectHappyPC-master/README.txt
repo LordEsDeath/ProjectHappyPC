336344502b234ada755502d69bceb41d7d00460b
LordEsDeath <50293185+LordEsDeath@users.noreply.github.com>
2023-06-07 22:54:34 +0300

    Delete ProjectHappyPC-master directory

--- ProjectHappyPC-master/README.txt
+++
@@ -1,3 +0,0 @@
-admin@news.ee 123456
-user@news.ee 123456
-moderator@news.ee 123456
--- ProjectHappyPC-master/resources/views/Users/regSuccess.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.appMain')
-
-@section('content')
-
-<div class="col-xs-12 col-sm-12 col-md-12 text-center">
-    <p>Регистрация успешна, выполните вход на сайт <a href="{{ url('/login') }}">Login</a>.</p>
-</div>
-
-@endsection
